6785d043ea24e99090c87b4cda91924d291756da
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-10-18 18:49:19 +0200

    modified testTrajet action

--- Flores_CERICAR/controller/mainController.php
+++ Flores_CERICAR/controller/mainController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Doctrine\ORM\NoResultException;
+
 class mainController
 {
 
@@ -22,6 +24,32 @@ class mainController
 			array_push($errors, 'Warning : param2 is missing :(');
 		}
 		$context->superTestErrors = $errors;
+		return context::SUCCESS;
+	}
+
+	public static function testTrajet($request, $context) {
+		$errors = [];
+
+		if($request['depart'] && $request['arrivee']) {
+			try {
+				$trajet = trajetTable::getTrajet($request['depart'], $request['arrivee']);
+			}
+			catch (NoResultException $e){
+				array_push($errors, "Il n'y a pas de trajet correspondant à vos critères. C'est triste.");
+				$context->testTrajetErrors = $errors;
+				return context::ERROR;
+			}
+
+			$context->foundTrajet = $trajet;
+		}
+		else {
+			array_push($errors, "L'arrivée ou le départ n'a pas été renseigné. Faites attention a l'avenir, c'est un peu cringe de déranger le serveur pour ça, il a déja assez de choses à gérer comme ça.");
+			$context->testTrajetErrors = $errors;
+			return context::ERROR;
+		}
+
+		$context->testTrajetErrors = $errors;
+
 		return context::SUCCESS;
 	}
 
