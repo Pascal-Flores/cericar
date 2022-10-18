430c96c0794f3b83cc3c10cceb55265cfe87bc9a
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-10-18 16:54:42 +0200

    modified superTest action

--- Flores_CERICAR/controller/mainController.php
+++ Flores_CERICAR/controller/mainController.php
@@ -10,9 +10,18 @@ class mainController
 	}
 
 	public static function superTest($request, $context) {
-		$context->param1 = $request['param1'];
-		$context->param2 = $request['param2'];
+		$errors = [];
+		if ($request['param1']) 
+			$context->param1 = $request['param1'];
+		else
+			array_push($errors, 'Warning : param1 is missing :(');
 
+		if($request['param2'])
+			$context->param2 = $request['param2'];
+		else {
+			array_push($errors, 'Warning : param2 is missing :(');
+		}
+		$context->superTestErrors = $errors;
 		return context::SUCCESS;
 	}
 
